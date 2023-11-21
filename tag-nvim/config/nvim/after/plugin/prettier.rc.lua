2a2582cb8e31ee600b0984c3a49cbcbc101b7b17
Vedran Hrncic <vedran.hrncic@lemonade.com>
2023-11-21 10:00:03 +0100

    Run prettier on save

--- tag-nvim/config/nvim/after/plugin/prettier.rc.lua
+++ tag-nvim/config/nvim/after/plugin/prettier.rc.lua
@@ -1,5 +1,16 @@
 local prettier = require('prettier')
 
+local group = vim.api.nvim_create_augroup("prettier-rb-format-on-save", { clear = false })
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group = group,
+  pattern = { "*.rb" },
+  callback = function(ev)
+    vim.api.nvim_command(':Prettier')
+  end,
+  desc = "[Prettier] format on save",
+})
+
 prettier.setup({
   bin = 'prettier',
   filetypes = {

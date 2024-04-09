3dc6f9a74a95296993bcb1357c71c8dfe0449468
Vedran Hrncic <vedran.hrncic@lemonade.com>
2024-04-09 14:39:49 +0200

    Update signature help keymap

--- tag-nvim-lazy/config/nvim/lua/vrabac/plugins/lsp.lua
+++ tag-nvim-lazy/config/nvim/lua/vrabac/plugins/lsp.lua
@@ -11,7 +11,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
     vim.keymap.set("n", "<leader>ld", vim.lsp.buf.definition, opts)
     vim.keymap.set("n", "<leader>lk", vim.lsp.buf.hover, opts)
     vim.keymap.set("n", "<leader>li", vim.lsp.buf.implementation, opts)
-    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+    vim.keymap.set("n", "<leader>lK", vim.lsp.buf.signature_help, opts)
     -- not sure what it does
     -- vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
     -- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)

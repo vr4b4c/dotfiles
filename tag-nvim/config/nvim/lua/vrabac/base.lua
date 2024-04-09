62a7b2bfdc200059ca620e48496a19ffb5257a89
Vedran Hrncic <vedran.hrncic@lemonade.com>
2024-04-09 14:42:38 +0200

    Disable nvim-packer providers for which checkhealth reports warning

--- tag-nvim/config/nvim/lua/vrabac/base.lua
+++ tag-nvim/config/nvim/lua/vrabac/base.lua
@@ -23,5 +23,7 @@ vim.opt.splitright = true
 vim.opt.splitbelow = true
 
 vim.g.mapleader = " "
-
-require('vrabac.macos') -- TODO: check the architecture
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0

eed3c6aa06897977c7b8f3dd593eda079a7d6106
Vedran Hrncic <vedran.hrncic@lemonade.com>
2024-04-09 12:26:48 +0200

    Configure nvim lsp

---
+++ tag-nvim-lazy/config/nvim/lua/vrabac/plugins/lsp.lua
@@ -0,0 +1,47 @@
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+    vim.keymap.set("n", "<leader>ld", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "<leader>lk", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "<leader>li", vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+    -- not sure what it does
+    -- vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
+    -- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set("n", "<leader>lw", function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+    vim.keymap.set("n", "<leader>lD", vim.lsp.buf.type_definition, opts)
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+    vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+    vim.keymap.set("n", "<leader>lr", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "<leader>lf", function()
+      vim.lsp.buf.format({ async = true })
+    end, opts)
+  end,
+})
+
+return {
+  "neovim/nvim-lspconfig",
+  config = function()
+    -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#lua_ls
+    local lspconfig = require("lspconfig")
+
+    lspconfig.lua_ls.setup({
+      on_init = function(client)
+        local path = client.workspace_folders[1].name
+        if vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc") then
+          return
+        end
+      end,
+    })
+    lspconfig.bashls.setup({})
+  end,
+}

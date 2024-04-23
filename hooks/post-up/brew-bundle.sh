312d3fcea79191b1deb02cc0db30cc886c45cd7c
Vedran Hrncic <vedran.hrncic@gmail.com>
2024-04-23 23:33:53 +0200

    Refactor Brewfile

--- hooks/post-up/brew-bundle.sh
+++ hooks/post-up/brew-bundle.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-if [ -f "$HOME/.Brewfile" ] && ! brew bundle check --global; then
+if [ -f "$HOME/.Brewfile" ] && ! brew bundle check --file="$HOME/.Brewfile"; then
   echo "[brew] Installing bundle dependencies"
-  brew bundle install 
+  brew bundle install --file="$HOME/.Brewfile"
+fi
+
+if [ -f "$HOME/.Brewfile.local" ] && ! brew bundle check --file="$HOME/.Brewfile.local"; then
+  echo "[brew/local] Installing bundle dependencies"
+  brew bundle install --file="$HOME/.Brewfile.local"
 fi

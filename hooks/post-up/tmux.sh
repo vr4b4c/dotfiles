740759189f021811e91c078b70afe01e742dc78c
Vedran Hrncic <vedran.hrncic@gmail.com>
2024-04-28 21:44:35 +0200

    Install tpm in post-up hook

--- hooks/post-up/tmux.sh
+++ hooks/post-up/tmux.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
 if [ -f "$HOME/.tmux.conf" ]; then
+  if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+    echo "[tmux] Installing tpm"
+    git clone https://github.com/tmux-plugins/tpm "$HOME/.tmux/plugins/tpm"
+  fi
+
   echo "[tmux] Installing plugins"
   bash "$HOME/.tmux/plugins/tpm/bin/install_plugins"
 fi

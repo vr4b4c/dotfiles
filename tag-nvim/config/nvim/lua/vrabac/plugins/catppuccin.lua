7c37a1caffb762477708539e8500406183299e51
Vedran Hrncic <vedran.hrncic@lemonade.com>
2024-05-10 09:04:39 +0200

    Refactor nvim switching

--- tag-nvim/config/nvim/lua/vrabac/plugins/catppuccin.lua
+++ tag-nvim/config/nvim/lua/vrabac/plugins/catppuccin.lua
@@ -5,19 +5,25 @@ return {
   config = function()
     local catppuccin = require("catppuccin")
 
-    if vim.env.V_THEME == "catppuccin-mocha" then
-      catppuccin.setup({
-        flavour = "mocha",
-        show_end_of_buffer = true,
-        transparent_background = true,
-      })
-      vim.cmd.colorscheme("catppuccin")
-    elseif vim.env.V_THEME == "catppuccin-latte" then
-      catppuccin.setup({
-        flavour = "latte",
-        show_end_of_buffer = true,
-        transparent_background = true,
-      })
+    local start, _, flavour = string.find(vim.env.V_THEME, "catppuccin%-(%a+)")
+    if start then
+      if flavour == "mocha" then
+        catppuccin.setup({
+          flavour = "mocha",
+          show_end_of_buffer = true,
+          transparent_background = true,
+        })
+        vim.cmd.colorscheme("catppuccin")
+      elseif flavour == "latte" then
+        catppuccin.setup({
+          flavour = "latte",
+          show_end_of_buffer = true,
+          transparent_background = true,
+        })
+      else
+        print(string.format("Unrecognized catppuccin flavour: %s", flavour))
+      end
+
       vim.cmd.colorscheme("catppuccin")
     end
   end,
--- tag-nvim/config/nvim/lua/vrabac/plugins/tokyo-night.lua
+++ tag-nvim/config/nvim/lua/vrabac/plugins/tokyo-night.lua
@@ -5,17 +5,22 @@ return {
   config = function()
     local tokyonight = require("tokyonight")
 
-    if vim.env.V_THEME == "tokyo-night-storm" then
-      tokyonight.setup({
-        style = "storm",
-        transparent = true,
-      })
-      vim.cmd.colorscheme("tokyonight")
-    elseif vim.env.V_THEME == "tokyo-night-day" then
-      tokyonight.setup({
-        style = "day",
-        transparent = false,
-      })
+    local start, _, style = string.find(vim.env.V_THEME, "tokyo%-night%-(%a+)")
+    if start then
+      if style == "storm" then
+        tokyonight.setup({
+          style = "storm",
+          transparent = true,
+        })
+      elseif style == "day" then
+        tokyonight.setup({
+          style = "day",
+          transparent = false,
+        })
+      else
+        print(string.format("Unrecognized tokyonight style: %s", style))
+      end
+
       vim.cmd.colorscheme("tokyonight")
     end
   end,

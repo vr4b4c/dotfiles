266ad8907f9e84a7a0a9ebb9b38b486d9c217d2f
Vedran Hrncic <vedran.hrncic@gmail.com>
2024-05-10 09:13:52 +0200

    Add support for eslint formatting and diagnostics

--- tag-nvim/config/nvim/lua/vrabac/plugins/none-ls.lua
+++ tag-nvim/config/nvim/lua/vrabac/plugins/none-ls.lua
@@ -1,11 +1,16 @@
 return {
   "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+  },
   config = function()
     local null_ls = require("null-ls")
 
     null_ls.setup({
       sources = {
         null_ls.builtins.formatting.stylua,
+        require("none-ls.diagnostics.eslint"),
+        require("none-ls.formatting.eslint"),
       },
     })
   end,

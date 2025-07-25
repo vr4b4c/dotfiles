5f16f2ed9d7440aa29df638e1899e728620cbafa
Vedran Hrncic <vedran.hrncic@gmail.com>
2025-07-25 15:23:13 +0200

    Add treesitter parsers for yaml and json

--- tag-nvim/config/nvim/lua/vrabac/plugins/treesitter.lua
+++ tag-nvim/config/nvim/lua/vrabac/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
     local configs = require("nvim-treesitter.configs")
 
     configs.setup({
-      ensure_installed = { "lua", "bash", "ruby", "terraform", "hcl" },
+      ensure_installed = { "lua", "bash", "ruby", "terraform", "hcl", "yaml", "json" },
       sync_install = false,
       highlight = { enable = true },
       indent = { enable = true },
